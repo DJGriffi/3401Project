2721519ff31393b5ba76dc063e3fa68758332869
Drew Griffiths <djamesg@mun.ca>
2022-10-31 08:32:15 -0230

    wtf am i doing???

--- decision-tree/program/growTree.py
+++ decision-tree/program/growTree.py
@@ -3,7 +3,8 @@ import math
 import numpy as np
 
 def main():
-    matrix = np.loadtxt("../data/train.txt")
+    # matrix = np.loadtxt("../data/train.txt")
+    matrix = np.loadtxt("/media/data/DrewSchool/3401Project/decision-tree/data/train.txt")
 
     # tree = 
     # print(matrix.shape)
@@ -26,14 +27,15 @@ def main():
     # print(unique_values)
     # print(occurrence_count)
 
-    with open("../data/dataDesc.txt") as f:
+    # with open("../data/dataDesc.txt") as f:
+    with open("/media/data/DrewSchool/3401Project/decision-tree/data/dataDesc.txt") as f: 
         attributeList = json.load(f)
     
     # print(attributeList)
 
     # m1 = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]])
     # a1 = [['RISK', [1,2]]]
-    generateTree(matrix, attributeList)
+    tree = generateTree(matrix, attributeList)
     # probabilities = occurrence_count / len(ageMatrix5[0])
     # print(probabilities)
     # print(calculateEntropy(ageMatrix))
@@ -87,8 +89,48 @@ def generateTree(matrix, attributeList):
         unique_values, occurrence_count = np.unique(matrix[[i]], return_counts=True)
         entropies.append(calculateEntropy(m, unique_values, occurrence_count))
 
-    print(entropies)
+    minEntropyIndex = np.argmin(entropies) + 1
+    testAttributeRow = matrix[[minEntropyIndex],:].tolist()
+    matrix = np.delete(matrix, minEntropyIndex, 0)
+    print(matrix)
+    node.append(attributeList[minEntropyIndex][0])
+    # node.append({})
+    # testAttributeDomain = [[],[1]]
+    testAttributeDomain = attributeList.pop(minEntropyIndex)
+    
+    print(len(testAttributeRow[0]))
+    # print(attributeList)
+    # print(entropies)
+    # print(attributeList)
+    # print(np.argmin(entropies))
+    print(node)
 
+    dict = {}
+    for child in testAttributeDomain[1]:
+        print("COPIED MATRIX")
+        matrixCopy = matrix.copy()
+        removeIndex = []
+        print(matrixCopy.shape)
+        for i in range(0, len(testAttributeRow[0])):
+            if (testAttributeRow[0][i] == child):
+                # matrixCopy.append(matrix[:,[i]].tolist())
+                removeIndex.append(i)
+        matrixCopy = np.delete(matrixCopy, removeIndex, 1)
+        print(matrixCopy.shape)
+        # matrixArr = np.array(matrixCopy)
+        # matrixArr = matrixArr.T
+        # matrixArr = np.delete(matrixArr, 0 , 0)
+        # matrixShape = matrixArr.shape
+        # print(matrixShape[1])
+        # np.reshape(matrixArr, (matrixShape[1], matrixShape[2]))
+        # print(matrixArr.shape)
+        
+        childNode = generateTree(matrixCopy, attributeList)
+        dict.update({child: childNode})
+
+    node.append(dict)
+    return node
+        # print(matrixArr)
 
 if __name__ == "__main__":
     main()
